6a051eeb0d1a3df94f02f82c754b9a13c290fd34
Noel Kuasmapa <noeljackkuasmapa@gmail.com>
2026-04-14 16:33:54 -0700

    Model link added updated

--- model/download_ml_model.py
+++ model/download_ml_model.py
@@ -1,6 +1,6 @@
 import gdown
 
-url = "https://drive.google.com/uc?id=FILE_ID"
+url = "https://drive.google.com/file/d/1M_wLBCuwTXSF5xRmQ68YIBKdoIuo7rww/view?usp=drive_link"
 output = "model.pt"
 
 gdown.download(url, output, quiet=False)
